27a0c39af4a2e63881a64cb147bbe7ca3ae9cffc
timur <timurka.zavyalov@mail.ru>
2023-02-14 19:55:28 +0500

    fix: changed a table in views

---
+++ crm/rn_stagehistories.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS crm.rn_stagehistories
+    (
+    `timezoneruleversionnumber` Nullable(Integer),
+    `versionnumber` Nullable(Integer),
+    `_owningbusinessunit_value` Nullable(String),
+    `rn_stagestep` Nullable(Integer),
+    `_owninguser_value` Nullable(String),
+    `statecode` Nullable(Integer),
+    `statuscode` Nullable(Integer),
+    `_createdby_value` Nullable(String),
+    `rn_name` Nullable(String),
+    `rn_stagehistoryid` Nullable(String),
+    `_ownerid_value` Nullable(String),
+    `modifiedon` Nullable(String),
+    `_rn_stageid_value` Nullable(String),
+    `_modifiedby_value` Nullable(String),
+    `_rn_opportunityid_value` Nullable(String),
+    `createdon` Nullable(Date),
+    `rn_eventdate` Nullable(Date),
+    `utcconversiontimezonecode` Nullable(String),
+    `overriddencreatedon` Nullable(String),
+    `importsequencenumber` Nullable(String),
+    `_modifiedonbehalfby_value` Nullable(String),
+    `_createdonbehalfby_value` Nullable(String),
+    `_owningteam_value` Nullable(String)
+    )
+ENGINE = MergeTree()
+ORDER BY versionnumber;
