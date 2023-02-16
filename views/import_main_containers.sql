a0e850a29907f2e882f7756a4d4e748e04815a3e
timur <timurka.zavyalov@mail.ru>
2023-02-16 15:31:21 +0500

    fix: changed a table in views

--- views/import_main_containers.sql
+++ views/import_main_containers.sql
@@ -15,7 +15,7 @@ FROM (
 	FROM (
 		SELECT import_id, ship_name, ship_name_unified, direction, is_empty, date, terminal, container_number, container_size, container_type, container_type_unified, goods_name_rus, consignment, count, teu, voyage, shipper_country, goods_weight, package_number, city, shipper_seaport, year, month, goods_tnved, parsed_on, year_parsed_on, month_parsed_on, consignee, consignee_inn, consignee_name_unified, seaport, seaport_unified, country, region, tnved_group_name, lat_port, long_port,
 		COUNT(*) OVER (PARTITION BY container_number, date, ship_name) AS count_dupl_cont_num
-		FROM import_enriched) AS t
+		FROM default.import_enriched) AS t
 	  WHERE t.count_dupl_cont_num > 1
   ) AS t2
 ) AS t3
