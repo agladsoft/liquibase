9744480b89abe501d48a13a6b9f667bfe2172d7a
timur <timurka.zavyalov@mail.ru>
2023-04-06 10:48:32 +0300

    git init

--- default/views/import_main_containers.sql
+++ default/views/import_main_containers.sql
@@ -8,12 +8,12 @@ count, count_main, is_empty, goods_name, tnved, tnved_group_name, goods_weight_n
 FROM (SELECT month_parsed_on, year_parsed_on, shipment_month, shipment_year, terminal, line_unified, ship_name_unified, voyage, shipment_date, consignment, min_consignment, direction, container_number, count_dupl_cont_num, container_size, container_type_unified, teu, count, 1 AS count_main, is_empty, goods_name, tnved, tnved_group_name, goods_weight_netto goods_weight_brutto, shipper_name, consignee_name, consignee_inn, consignee_name_unified, city, tracking_country_unified, tracking_seaport_unified, region, lat_port, long_port
 FROM (
   SELECT month_parsed_on, year_parsed_on, shipment_month, shipment_year, terminal, line_unified, ship_name_unified, voyage, shipment_date, consignment, direction, container_number, count_dupl_cont_num, container_size, container_type_unified, teu, count, is_empty, goods_name, tnved, tnved_group_name, goods_weight_netto goods_weight_brutto, shipper_name, consignee_name, consignee_inn, consignee_name_unified, city, tracking_country_unified, tracking_seaport_unified, region, lat_port, long_port,
-  MIN(consignment) OVER (PARTITION BY container_number, shipment_date, ship_name) AS min_consignment
+  MIN(consignment) OVER (PARTITION BY container_number, shipment_date, ship_name_unified) AS min_consignment
   FROM (
 	SELECT month_parsed_on, year_parsed_on, shipment_month, shipment_year, terminal, line_unified, ship_name_unified, voyage, shipment_date, consignment, direction, container_number, count_dupl_cont_num, container_size, container_type_unified, teu, count, is_empty, goods_name, tnved, tnved_group_name, goods_weight_netto goods_weight_brutto, shipper_name, consignee_name, consignee_inn, consignee_name_unified, city, tracking_country_unified, tracking_seaport_unified, region, lat_port, long_port
 	FROM (
 		SELECT month_parsed_on, year_parsed_on, shipment_month, shipment_year, terminal, line_unified, ship_name_unified, voyage, shipment_date, consignment, direction, container_number, container_size, container_type_unified, teu, count, is_empty, goods_name, tnved, tnved_group_name, goods_weight_netto goods_weight_brutto, shipper_name, consignee_name, consignee_inn, consignee_name_unified, city, tracking_country_unified, tracking_seaport_unified, region, lat_port, long_port,
-		COUNT(*) OVER (PARTITION BY container_number, shipment_date, ship_name) AS count_dupl_cont_num
+		COUNT(*) OVER (PARTITION BY container_number, shipment_date, ship_name_unified) AS count_dupl_cont_num
 		FROM default.import_enriched) AS t
 	  WHERE t.count_dupl_cont_num > 1
   ) AS t2
