de46a2f243c283ef8c99c397c3c28f338299b200
timur <timurka.zavyalov@mail.ru>
2023-05-26 10:36:24 +0300

    fix: fixed swap in values in function

--- rzhd/functions/swap_values.sql
+++ rzhd/functions/swap_values.sql
@@ -1,7 +1,13 @@
 CREATE OR REPLACE FUNCTION swap_values AS (attendee_by_puzt, attendee_according_to_egrpo, default_value) ->
 multiIf(
-    toInt64OrNull(attendee_by_puzt) is not null,
+    toInt64OrNull(attendee_by_puzt) is not null and (attendee_according_to_egrpo is null
+        or attendee_according_to_egrpo = 'неизвестен ОКПО' or attendee_according_to_egrpo = 'неизвестен'
+        or toInt64OrNull(attendee_according_to_egrpo) is not null),
     default_value,
+    toInt64OrNull(attendee_by_puzt) is not null and not (attendee_according_to_egrpo is null
+        or attendee_according_to_egrpo = 'неизвестен ОКПО' or attendee_according_to_egrpo = 'неизвестен'
+        or toInt64OrNull(attendee_according_to_egrpo) is not null),
+    attendee_according_to_egrpo,
     (attendee_by_puzt is null or attendee_by_puzt = 'неизвестен' or attendee_by_puzt = 'НЕИЗВЕСТЕН')
         and (attendee_according_to_egrpo is null
         or attendee_according_to_egrpo = 'неизвестен ОКПО' or attendee_according_to_egrpo = 'неизвестен'
@@ -19,7 +25,9 @@ multiIf(
         or attendee_according_to_egrpo = 'неизвестен ОКПО' or attendee_according_to_egrpo = 'неизвестен'
         or toInt64OrNull(attendee_according_to_egrpo) is not null),
     attendee_according_to_egrpo,
-    attendee_by_puzt is not null or attendee_by_puzt != 'НЕИЗВЕСТЕН' or attendee_by_puzt != 'неизвестен',
-    attendee_by_puzt,
-    attendee_according_to_egrpo
+    attendee_by_puzt is null and not (attendee_according_to_egrpo is null
+        or attendee_according_to_egrpo = 'неизвестен ОКПО' or attendee_according_to_egrpo = 'неизвестен'
+        or toInt64OrNull(attendee_according_to_egrpo) is not null),
+    attendee_according_to_egrpo,
+    attendee_by_puzt
 );
