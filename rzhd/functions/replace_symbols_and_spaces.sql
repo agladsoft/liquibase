d36fbf526ea410e20558aa02b344a69e3018b2bf
timur <timurka.zavyalov@mail.ru>
2023-06-09 13:34:14 +0300

    fix: improved sql-scpript proccessing time

--- rzhd/functions/replace_symbols_and_spaces.sql
+++ rzhd/functions/replace_symbols_and_spaces.sql
@@ -1,2 +1,2 @@
 CREATE OR REPLACE FUNCTION replace_symbols_and_spaces AS (payer_of_the_railway_tariff) ->
-trimBoth(replaceRegexpAll(replaceRegexpAll(upperUTF8(payer_of_the_railway_tariff), '[",?«]', ''), ' +', ' '));
+trimBoth(replaceRegexpAll(replaceRegexpAll(upperUTF8(payer_of_the_railway_tariff), '[",?«].', ''), ' +', ' '));
