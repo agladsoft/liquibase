76a6b4554cfae2fb534416ebcd660303f98cc0b2
timur <timurka.zavyalov@mail.ru>
2023-08-03 18:05:46 +0300

    add: added new fields (morservice, spardeck)

--- default/cross_check/import_enriched.sql
+++ default/cross_check/import_enriched.sql
@@ -21,7 +21,7 @@ AS SELECT
     import.teu AS teu,
     import.container_count AS container_count,
         CASE
-            WHEN re.is_empty = import.goods_name THEN true
+            WHEN re.is_empty = import.goods_name and goods_weight_with_package = 0 THEN true
             ELSE false
         END AS is_empty,
     import.goods_name AS goods_name,
