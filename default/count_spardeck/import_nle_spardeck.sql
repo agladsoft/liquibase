984383e7e6acd6d24c3abe33cf47881558b27cbd
timur <timurka.zavyalov@mail.ru>
2023-08-10 14:58:13 +0300

    add: added linear counting

--- default/count_spardeck/import_nle_spardeck.sql
+++ default/count_spardeck/import_nle_spardeck.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW default.import_nle_spardeck
 AS SELECT line_unified, ship_name_unified, shipment_date, month_parsed_on, year_parsed_on, count(*) AS count_container
 FROM import_enriched
-WHERE terminal = 'НЛЭ' AND month_parsed_on = 6 AND year_parsed_on = 2023
+WHERE terminal = 'НЛЭ' AND month_parsed_on = 5 AND year_parsed_on = 2023
 GROUP BY line_unified, ship_name_unified, shipment_date, month_parsed_on, year_parsed_on
 ORDER BY line_unified
