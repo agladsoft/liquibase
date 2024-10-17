30f387edea209fdda5edaba2767252a0857c4979
timur <timurzavalov@gmail.com>
2024-10-17 15:59:29 +0300

    add: added materialized view

--- default/count_spardeck/all_enriched_live_line_report.sql
+++ default/count_spardeck/all_enriched_live_line_report.sql
@@ -1,4 +1,71 @@
-CREATE LIVE VIEW IF NOT EXISTS default.all_enriched_live_line_report WITH REFRESH 86400
-AS SELECT * FROM default.all_enriched_line_report
+CREATE MATERIALIZED VIEW default.all_enriched_live_line_report
+REFRESH AFTER 30 MINUTE
+(
+
+    `month` Int32,
+
+    `year` Int32,
+
+    `direction` Nullable(String),
+
+    `terminal` Nullable(String),
+
+    `line_unified` Nullable(String),
+
+    `ship_name_unified` Nullable(String),
+
+    `voyage` Nullable(String),
+
+    `shipment_date` Nullable(Date),
+
+    `container_number` Nullable(String),
+
+    `container_size` Nullable(Int32),
+
+    `container_type_unified` Nullable(String),
+
+    `teu` Nullable(Int32),
+
+    `container_count` Nullable(Int32),
+
+    `is_empty` Nullable(Bool),
+
+    `ref` Nullable(String),
+
+    `goods_name` Nullable(String),
+
+    `tnved_group_id` Nullable(String),
+
+    `tnved_group_name` Nullable(String),
+
+    `goods_weight_with_package` Nullable(Decimal(38,
+ 19)),
+
+    `company` Nullable(String),
+
+    `company_inn` Nullable(String),
+
+    `company_unified` Nullable(String),
+
+    `expeditor` Nullable(String),
+
+    `tracking_country_unified` Nullable(String),
+
+    `tracking_seaport_unified` Nullable(String),
+
+    `region` Nullable(String),
+
+    `lat_port` Nullable(Float32),
+
+    `long_port` Nullable(Float32),
+
+    `geopolygon` Nullable(String),
+
+    `geopoint` Nullable(String)
+)
+ENGINE = MergeTree
+ORDER BY month
+POPULATE AS
+SELECT * FROM default.all_enriched_line_report
 SETTINGS
-allow_experimental_live_view = 1
+allow_experimental_refreshable_materialized_view=1
